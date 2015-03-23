41570c765daa0a83430c354c6605ce84cd8950d4
Diogo Leitão <diogo.n.leitao@gmail.com>
2015-03-23 17:52:01 +0000

    Privileges changed to Class entity with 4 booleans to map access privileges

--- app/src/main/java/pt/utl/ist/airdesk/domain/Privileges.java
+++ app/src/main/java/pt/utl/ist/airdesk/domain/Privileges.java
@@ -3,6 +3,50 @@ package pt.utl.ist.airdesk.domain;
 /**
  * Created by Diogo on 18/03/2015.
  */
-public enum Privileges {
-    READ, WRITE, CREATE, DELETE;
+public class Privileges {
+	private boolean read;
+	private boolean write;
+	private boolean create;
+	private boolean delete;
+
+	public Privileges() {}
+
+	public Privileges(boolean read, boolean write, boolean create, boolean delete) {
+		this.read = read;
+		this.write = write;
+		this.create = create;
+		this.delete = delete;
+	}
+
+	public boolean canRead() {
+		return this.read;
+	}
+
+	public boolean canWrite() {
+		return this.write;
+	}
+
+	public boolean canCreate() {
+		return this.create;
+	}
+
+	public boolean canDelete() {
+		return this.delete;
+	}
+
+	public void setReadPrivilege(boolean read) {
+		this.read = read;
+	}
+
+	public void setWritePrivilege(boolean write) {
+		this.write = write;
+	}
+
+	public void setCreatePrivilege(boolean create) {
+		this.create = create;
+	}
+
+	public void setDeletePrivilege(boolean delete) {
+		this.delete = delete;
+	}
 }
