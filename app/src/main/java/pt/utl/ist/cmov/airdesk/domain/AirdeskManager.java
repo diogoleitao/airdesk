1bb8446a7c49437e4d3e14b55dd6b3d7a9526f6f
Diogo Leitão <diogo.n.leitao@gmail.com>
2015-03-25 16:06:45 +0000

    Added File class

--- app/src/main/java/pt/utl/ist/cmov/airdesk/domain/AirdeskManager.java
+++ app/src/main/java/pt/utl/ist/cmov/airdesk/domain/AirdeskManager.java
@@ -8,25 +8,34 @@ public class AirdeskManager {
     private static AirdeskManager instance = null;
     private HashMap<String, User> registeredUsers = new HashMap<String, User>();
     private HashMap<String, Workspace> workspaces = new HashMap<String, Workspace>();
+    private HashMap<String, ArrayList<File>> workspaceFiles = new HashMap<String, ArrayList<File>>();
 
-    public AirdeskManager() {
+    private AirdeskManager() {}
+
+    public AirdeskManager getInstance() {
         if (instance == null) {
             instance = new AirdeskManager();
         }
+        return  instance;
     }
 
     public void addUser(String name, String nickname, String email) throws Exception {
         User user = new User(name, nickname, email);
-        if (getUserByNickname(nickname).equals("")) {
+        if (getUserByNickname(nickname).equals(null)) {
             registeredUsers.put(nickname, user);
         } else {
             throw new Exception();
         }
     }
 
-    public String getUserByNickname(String nickname) {
+    public void addWorkspace(String nickname, String workspace) {
+        User user = getUserByNickname(nickname);
+        user.createWorkspace(0, workspace);
+    }
+
+    public User getUserByNickname(String nickname) {
         if (registeredUsers.keySet().contains(nickname)) {
-            return nickname;
+            return registeredUsers.get(nickname);
         } else {
             return null;
         }
