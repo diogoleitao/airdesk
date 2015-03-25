2e0ae9272a912fdeb58a01236ff8a6bf7e03a1e5
Diogo Leitão <diogo.n.leitao@gmail.com>
2015-03-25 16:15:22 +0000

    Finally got rid of annoying files and everything is ok

--- app/src/main/java/pt/utl/ist/cmov/airdesk/domain/AirdeskManager.java
+++ app/src/main/java/pt/utl/ist/cmov/airdesk/domain/AirdeskManager.java
@@ -8,13 +8,6 @@ public class AirdeskManager {
     private static AirdeskManager instance = null;
     private HashMap<String, User> registeredUsers = new HashMap<String, User>();
     private HashMap<String, Workspace> workspaces = new HashMap<String, Workspace>();
-<<<<<<< HEAD
-
-    public AirdeskManager() {
-        if (instance == null) {
-            instance = new AirdeskManager();
-        }
-=======
     private HashMap<String, ArrayList<File>> workspaceFiles = new HashMap<String, ArrayList<File>>();
 
     private AirdeskManager() {}
@@ -24,27 +17,17 @@ public class AirdeskManager {
             instance = new AirdeskManager();
         }
         return  instance;
->>>>>>> 1bb8446a7c49437e4d3e14b55dd6b3d7a9526f6f
     }
 
     public void addUser(String name, String nickname, String email) throws Exception {
         User user = new User(name, nickname, email);
-<<<<<<< HEAD
-        if (getUserByNickname(nickname).equals("")) {
-=======
         if (getUserByNickname(nickname).equals(null)) {
->>>>>>> 1bb8446a7c49437e4d3e14b55dd6b3d7a9526f6f
             registeredUsers.put(nickname, user);
         } else {
             throw new Exception();
         }
     }
 
-<<<<<<< HEAD
-    public String getUserByNickname(String nickname) {
-        if (registeredUsers.keySet().contains(nickname)) {
-            return nickname;
-=======
     public void addWorkspace(String nickname, String workspace) {
         User user = getUserByNickname(nickname);
         user.createWorkspace(0, workspace);
@@ -53,7 +36,6 @@ public class AirdeskManager {
     public User getUserByNickname(String nickname) {
         if (registeredUsers.keySet().contains(nickname)) {
             return registeredUsers.get(nickname);
->>>>>>> 1bb8446a7c49437e4d3e14b55dd6b3d7a9526f6f
         } else {
             return null;
         }
